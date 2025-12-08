0d65cc04f5ea124a45b2c04716991b0050558258
Shawn. TianLing <wegtery9@outlook.com>
2025-12-08 20:31:11 +0800

    Change default IP address in diy-part2 script
    Updated the script to change the default IP address from 192.168.1.1 to 192.168.2.1.

--- rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
+++ rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
@@ -11,6 +11,9 @@
 #
 
 # Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
