3932fac09e018dda81801148063a6224021cd576
Shawn. TianLing <wegtery9@outlook.com>
2025-12-08 20:37:17 +0800

    Update diy-part2-rax3000m.sh

--- rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
+++ rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
@@ -12,8 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
 
