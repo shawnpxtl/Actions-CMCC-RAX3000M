0a41ca70c76d2a6f703164aa3e05d56e72dbe27a
Shawn. TianLing <wegtery9@outlook.com>
2026-07-03 19:07:36 +0800

    Update sysupgrade.conf path and modify default IP

--- rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
+++ rax3000m/imoutowrt-237/diy-part2-rax3000m.sh
@@ -11,7 +11,7 @@
 #
 
 # Add backup file and 
-echo '/etc/nikki/run/geoip.metadb' >> /etc/sysupgrade.conf
+echo '/etc/nikki/run/geoip.metadb' >> package/base-files/files/etc/sysupgrade.conf
 
 # Modify default IP and password
 sed -i 's/192.168.6.1/192.168.2.1/g' package/base-files/files/bin/config_generate
